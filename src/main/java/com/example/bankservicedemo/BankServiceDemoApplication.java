6d61ca8dc513d31d9a5db26ce1701306113eb9b8
antonpanchishko <PanchishkoMayor@gmail.com>
2021-02-28 16:10:42 +0200

    first commit

---
+++ src/main/java/com/example/bankservicedemo/BankServiceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankServiceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankServiceDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bankservicedemo/BankServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankServiceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
