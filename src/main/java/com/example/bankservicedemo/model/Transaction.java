2e860096a82c699a82521376ea4b3634f50ed5d0
antonpanchishko <PanchishkoMayor@gmail.com>
2021-03-01 22:43:26 +0200

    11) renamed columns in Transaction model

--- src/main/java/com/example/bankservicedemo/model/Transaction.java
+++ src/main/java/com/example/bankservicedemo/model/Transaction.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.validation.constraints.Min;
 import lombok.Data;
@@ -16,8 +17,10 @@ public class Transaction {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @OneToOne
+    @JoinColumn(name = "account_from")
     private Account accountFrom;
     @OneToOne
+    @JoinColumn(name = "account_to")
     private Account accountTo;
     @Min(0)
     private double amount;
