3d82fc92628d39d82b05d3b409e9eb44e7d129d8
antonpanchishko <PanchishkoMayor@gmail.com>
2021-03-03 11:34:26 +0200

    15) added client service and added dependency in pom

---
+++ src/main/java/com/example/bankservicedemo/service/ClientService.java
@@ -0,0 +1,34 @@
+package com.example.bankservicedemo.service;
+
+import com.example.bankservicedemo.model.Currency;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.LocalDate;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.common.DataProcessingException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class ClientService {
+    private double getRate(LocalDate date, Currency currencyFrom, Currency currencyTo) {
+        String path = "https://api.exchangerate.host/convert?from=" + currencyFrom.toString()
+                + "&to=" + currencyTo.toString() + "&date=" ;
+        try {
+            URL url = new URL(path);
+            HttpURLConnection request = (HttpURLConnection) url.openConnection();
+            request.connect();
+            JsonElement root = JsonParser
+                    .parseReader(new InputStreamReader((InputStream) request.getContent()));
+            JsonObject jsonObject = root.getAsJsonObject();
+            return jsonObject.get("result").getAsDouble();
+        } catch (IOException e) {
+            throw new DataProcessingException("Can't get connection to the external API", e);
+        }
+    }
+}
